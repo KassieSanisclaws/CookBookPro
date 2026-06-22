f7e335363213c5ac05a37015b2aca00bf0b1cdd9
Kadeem Cherman <Kassie35dev@gmail.com>
2026-06-22 16:14:29 -0600

    Initial Commit

---
+++ CookBookPro/CookBookProApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookBookProApp.swift
+//  CookBookPro
+//
+//  Created by Kadeem Cherman on 2026-06-22.
+//
+
+import SwiftUI
+
+@main
+struct CookBookProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
